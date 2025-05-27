3bf5b45a889da8f7fbba7e48380638a805562787
Mounika <kasireddymounika300@gmail.com>
2025-05-27 16:14:22 +0530

    first commit

---
+++ javaDay1/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld{
+   public static void main(String[] args){
+
+   }
+}
+
+
+
+
+
+
+
+
+
+// / code runner
+// java by red hat
+// to compile a file the command is javac FILENAME.java
+// to run the byte file java CLASSNAME
---
+++ javaDay1/Operator.java
@@ -0,0 +1,18 @@
+public class Operator{
+    public static void main(String[] args){
+       /* Operators
+        1. Arithmetic-  - + % * / //
+        2. relational- 
+        3. assignment- += -= 
+        4.logical  && || ! 
+        5.bitwise - & | ^ ~ << >> >>>
+        6. unary= ++ ,--
+        7. ternary -  (condition)? T : F ;
+        8. comma 
+        */
+       int x=5;
+       System.out.println(~5);
+
+
+    }
+}
